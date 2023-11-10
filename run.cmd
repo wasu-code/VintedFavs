05cbdbe015b7055df46577a2e02fe35a24725e69
wasu-code <61418403+wasu-code@users.noreply.github.com>
2023-11-10 12:53:11 +0100

    autorun file

--- run.cmd
+++ run.cmd
@@ -1,15 +1,17 @@
+cd public
+
 :: Start XAMPP
-D:/xampp/xampp_start.exe
+xampp_start
 
 :: Start PHP server
 start "PHP Server" cmd /c "php -S localhost:8000"
 
 :: Open Browser
-start "" "localhost:8000"
+start "" "http://localhost:8000"
 
 :: Pause the current command window
 echo Do you want to close XAMPP?
 pause
 
 :: Stop XAMPP
-D:/xampp/xampp_stop.exe
+xampp_stop

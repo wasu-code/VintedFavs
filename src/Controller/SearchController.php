a2184b756510a9a05b5cc52173a4bf1f4a9b173d
Szymon Warszawski <szymonwarszawski4@gmail.com>
2023-11-11 14:18:59 +0100

    Update SearchController.php

--- src/Controller/SearchController.php
+++ src/Controller/SearchController.php
@@ -5,69 +5,73 @@ namespace App\Controller;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 
-
 class SearchController extends VintedController
 {
-  public function default(Request $request, Response $response, array $args)
-  {
-    //Get Params
-    $currentPage = $request->getQueryParam('page');
-    if ($currentPage) {
-      $args['cnt'] = $currentPage;
-    }
-    $sort = $request->getQueryParam('sort');
-    if ($sort && $sort != 'fav') {
-      $args['order'] = $sort; //relevance; newest_first; price_low_to_high; price_high_to_low
-    }
+    public function default(Request $request, Response $response, array $args)
+    {
+        // Pobieranie parametrów z zapytania
+        $currentPage = $request->getQueryParam('page');
+        $sort = $request->getQueryParam('sort');
+        $query = $args['query'];
 
-    if ($sort == 'fav') {
-      //sortowanie po ulub
-      $cookie = $this->get_cookie("https://www.vinted.pl/");
-      $data = json_decode($this->get_web_page('https://www.vinted.pl/api/v2/catalog/items?order=favourite_count&per_page=900', '_vinted_fr_session=' . $cookie), true);
-      $items = $data["items"];
-      $page = 1;
-      $max_pages = $data["pagination"]["total_pages"];
+        // Domyślne ustawienia
+        $args['cnt'] = $currentPage ?? 1;
+        $args['order'] = $sort && $sort !== 'fav' ? $sort : 'relevance';
 
-      while ($page <= $max_pages) {
-        $dat = json_decode($this->get_web_page('https://www.vinted.pl/api/v2/catalog/items?order=favourite_count&per_page=900&page=' . $page, '_vinted_fr_session=' . $cookie), true);
-        $page++;
-        $items = array_merge($items, $dat["items"]);
-      }
-
-      print_r(count($items));
-      print_r($data['pagination']);
-      //sortowanie, które wcześniej zrobiłem po $items
-      //wybranie 46 pierwszych itemów z listy
-      //wyświetlenie ich
-      usort($items, function ($a, $b) {
-        if ($a["favourite_count"] == $b["favourite_count"]) {
-          return 0;
+        // Sortowanie po ulubionych
+        if ($sort == 'fav') {
+            $items = $this->getFavoriteItems($query);
+        } else {
+            // Wyszukiwanie z uwzględnieniem frazy
+            $vinted = $this->search($request, $response, $args);
+            $items = $vinted['items'];
         }
-        return ($a["favourite_count"] > $b["favourite_count"]) ? -1 : 1;
-      });
-    } else {
-      $vinted = $this->search($request, $response, $args);
-      $pagination = $vinted['pagination'];
-      //var_dump($pagination);
-      $items = $vinted['items'];
+
+        // Wyświetlanie wyników
+        return $this->render($response, 'search.html', [
+            'items' => array_slice($items, 0, 46), // Wybieranie 46 pierwszych elementów
+            'query' => $query,
+            'page' => $currentPage ?? 1
+        ]);
     }
 
-
-    if (!$currentPage) {
-      $currentPage = 1;
+    public function homepage(Request $request, Response $response)
+    {
+        // Wyświetlanie strony domowej
+        return $this->render($response, 'search.html', []);
     }
 
-    return $this->render($response, 'search.html', [  // render response using 'search.html' template with data of 'items'
-      'items' => $items,
-      'query' => $args['query'],
-      'page' => $currentPage
-    ]);
-  }
+    private function getFavoriteItems($query)
+    {
+        $cookie = $this->get_cookie("https://www.vinted.pl/");
+      
+        $data = json_decode($this->get_web_page('https://www.vinted.pl/api/v2/catalog/items?order=favourite_count&per_page=900&search_text=' . $query, '_vinted_fr_session=' . $cookie), true);
+        if (!isset($data['items'])) {
+            return [];
+        }
 
-  public function homepage(Request $request, Response $response)
-  {
-    return $this->render($response, 'search.html', [  // render response using 'search.html' template with data of 'items'
+        $items = $data['items'];
 
-    ]);
-  }
+        $page = 2; // Początkowa strona (pierwsza została już pobrana)
+        $maxPages = $data['pagination']['total_pages'];
+
+        // Pobieranie kolejnych stron, ale nie więcej niż 5, aby uniknąć zbyt wielu zapytań
+        while ($page <= $maxPages && $page <= 5) {
+            $data = json_decode($this->get_web_page("https://www.vinted.pl/api/v2/catalog/items?order=favourite_count&per_page=900&page={$page}&q={$query}", '_vinted_fr_session=' . $cookie), true);
+
+            if (!isset($data['items'])) {
+                break;
+            }
+
+            $items = array_merge($items, $data['items']);
+            $page++;
+        }
+
+        // Sortowanie po ulubionych
+        usort($items, function ($a, $b) {
+            return $b["favourite_count"] - $a["favourite_count"];
+        });
+
+        return $items;
+    }
 }
